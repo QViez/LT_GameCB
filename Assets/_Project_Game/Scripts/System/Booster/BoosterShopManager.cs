edb46377e909af99b2345c2e5349ebe748bc5b63
QViez <vietyknb470@gmail.com>
2026-09-14 18:52:39 +0700

    .

--- Assets/_Project_Game/Scripts/System/Booster/BoosterShopManager.cs
+++ Assets/_Project_Game/Scripts/System/Booster/BoosterShopManager.cs
@@ -7,14 +7,36 @@ public class BoosterShopManager : MonoBehaviour
 
     public event Action OnBoosterPurchased;
 
+    [Header("Booster mặc định")]
+    public BoosterSO[] boosters;
+    public int defaultBoosterCount = 3;
+
     private void Awake()
     {
         Instance = this;
+
+        InitializeBoosters();
+    }
+    private void InitializeBoosters()
+    {
+        foreach (BoosterSO booster in boosters)
+        {
+            if (booster == null) continue;
+
+            string key = $"BOOSTER_{booster.boosterID}";
+
+            if (!PlayerPrefs.HasKey(key))
+            {
+                PlayerPrefs.SetInt(key, defaultBoosterCount);
+            }
+        }
+
+        PlayerPrefs.Save();
     }
 
     public int GetBoosterCount(string boosterID)
     {
-        return PlayerPrefs.GetInt($"BOOSTER_{boosterID}", 0);
+        return PlayerPrefs.GetInt($"BOOSTER_{boosterID}", defaultBoosterCount);
     }
 
     public bool BuyBoosterDirectly(BoosterSO booster)
@@ -29,16 +51,18 @@ public class BoosterShopManager : MonoBehaviour
         if (CurrencyManager.Instance.TrySpendCoins(booster.price))
         {
             int currentAmount = GetBoosterCount(booster.boosterID);
-            PlayerPrefs.SetInt($"BOOSTER_{booster.boosterID}", currentAmount + 1);
+
+            PlayerPrefs.SetInt($"BOOSTER_{booster.boosterID}",currentAmount + 1);
+
             PlayerPrefs.Save();
 
-            Debug.Log($"<color=green>Mua thành công 1 {booster.boosterName}! Số lượng hiện có: {currentAmount + 1}</color>");
+            Debug.Log("Mua thành công 1 {booster.boosterName}! " +"Số lượng hiện có: {currentAmount + 1}");
 
             OnBoosterPurchased?.Invoke();
             return true;
         }
 
-        Debug.LogWarning("Không đủ tiền ");
+        Debug.LogWarning("Không đủ tiền");
         return false;
     }
 }
